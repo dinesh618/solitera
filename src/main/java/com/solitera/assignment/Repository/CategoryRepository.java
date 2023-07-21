e02142459ef72a9b29c2f0f47340e6d295bf1f95
Dinesh Chauhan <dinesh@bucle.dev>
2023-07-21 17:26:28 +0530

    do some changes

--- src/main/java/com/solitera/assignment/Repository/CategoryRepository.java
+++ src/main/java/com/solitera/assignment/Repository/CategoryRepository.java
@@ -1,10 +1,16 @@
 package com.solitera.assignment.Repository;
 
 import com.solitera.assignment.model.Category;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
+
     Category findByCategory(String category);
+
 }
